f647a35cddbc8a7d95c6b951f3a66c56068f968a
Danilo <danilogmoura@gmail.com>
2021-02-09 17:33:08 -0300

    3.16 Desafio: Lombok e repositório de restaurantes

---
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Component
+public class RestauranteRepositoryImpl implements RestauranteRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Restaurante> todos() {
+        return entityManager
+                .createQuery("select r from Restaurante r", Restaurante.class)
+                .getResultList();
+    }
+
+    @Override
+    public Restaurante porId(Long id) {
+        return entityManager.find(Restaurante.class, id);
+    }
+
+    @Override
+    @Transactional
+    public Restaurante adicionar(Restaurante restaurante) {
+        return entityManager.merge(restaurante);
+    }
+
+    @Override
+    @Transactional
+    public void remover(Restaurante restaurante) {
+        restaurante = porId(restaurante.getId());
+        entityManager.remove(restaurante);
+    }
+}
--- algafood-api/src/main/resources/import.sql
+++ algafood-api/src/main/resources/import.sql
@@ -1,2 +1,5 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+
+insert into restaurante (nome, taxa_frete) values ('Frigideira', 3.44);
+insert into restaurante (nome, taxa_frete) values ('Italian Food', 19.0);
