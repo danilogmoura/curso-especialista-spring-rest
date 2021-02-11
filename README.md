163e6ba5989cc050640b590a00d8b2887ecc0242
Danilo <danilogmoura@gmail.com>
2021-02-11 10:32:05 -0300

    4.21 Corrigindo o Status HTTP para resource inexistente

--- README.md
+++ README.md
@@ -95,7 +95,7 @@
 - [x] 4.18 Conhecendo os códigos de status HTTP                                                                   8m 14s
 - [x] 4.19 Definindo o status da resposta HTTP com @ResponseStatus                                                1m 47s
 - [x] 4.20 Manipulando a resposta HTTP com ResponseEntity                                                         10m 10s
-- [ ] 4.21 Corrigindo o Status HTTP para resource inexistente                                                     4m 46s
+- [x] 4.21 Corrigindo o Status HTTP para resource inexistente                                                     4m 46s
 - [ ] 4.22 Status HTTP para collection resource vazia: qual usar?                                                 3m 36s
 - [ ] 4.23 Modelando e implementando a inclusão de recursos com POST                                              12m 7s
 - [ ] 4.24 Negociando o media type do payload do POST com Content-Type                                            3m 55s
--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,7 +4,6 @@ import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -34,16 +33,12 @@ public class CozinhaController {
     @GetMapping("/{cozinhaId}")
     public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
         Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
-//        return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-//        return ResponseEntity.ok(cozinha);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+        if (cozinha != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+        }
 
-        return ResponseEntity
-                .status(HttpStatus.FOUND)
-                .headers(headers)
-                .build();
+        return ResponseEntity.notFound().build();
     }
 
     @Autowired
