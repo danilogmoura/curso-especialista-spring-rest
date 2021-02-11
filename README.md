8d0d90398eb58fda2688507ba1b73b1407bf0724
Danilo <danilogmoura@gmail.com>
2021-02-11 10:59:34 -0300

    4.23 Modelando e implementando a inclusão de recursos com POST

--- README.md
+++ README.md
@@ -97,7 +97,7 @@
 - [x] 4.20 Manipulando a resposta HTTP com ResponseEntity                                                         10m 10s
 - [x] 4.21 Corrigindo o Status HTTP para resource inexistente                                                     4m 46s
 - [x] 4.22 Status HTTP para collection resource vazia: qual usar?                                                 3m 36s
-- [ ] 4.23 Modelando e implementando a inclusão de recursos com POST                                              12m 7s
+- [x] 4.23 Modelando e implementando a inclusão de recursos com POST                                              12m 7s
 - [ ] 4.24 Negociando o media type do payload do POST com Content-Type                                            3m 55s
 - [ ] 4.25 Modelando e implementando a atualização de recursos com PUT                                            13m 31s
 - [ ] 4.26 Modelando e implementando a exclusão de recursos com DELETE                                            11m 1s
--- algafood-api/src/main/resources/import.sql
+++ algafood-api/src/main/resources/import.sql
@@ -1,13 +1,19 @@
 insert into cozinha (id, nome) values (1, 'Tailandesa');
 insert into cozinha (id, nome) values (2, 'Indiana');
 
+SELECT setval('cozinha_id_seq', 2, true);
+
 insert into restaurante (id, nome, taxa_frete, cozinha_id) values (1, 'Thai Gourmet', 10, 1);
 insert into restaurante (id, nome, taxa_frete, cozinha_id) values (2, 'Thai Delivery', 9.50, 1);
 insert into restaurante (id, nome, taxa_frete, cozinha_id) values (3, 'Tuk Tuk Comida Indiana', 15, 2);
 
+SELECT setval('restaurante_id_seq', 3, true);
+
 insert into estado (id, nome) values (1, 'Minas Gerais');
 insert into estado (id, nome) values (2, 'São Paulo');
 insert into estado (id, nome) values (3, 'Ceará');
+
+SELECT setval('estado_id_seq', 3, true);
 
 insert into cidade (id, nome, estado_id) values (1, 'Uberlândia', 1);
 insert into cidade (id, nome, estado_id) values (2, 'Belo Horizonte', 1);
@@ -15,9 +21,15 @@ insert into cidade (id, nome, estado_id) values (3, 'São Paulo', 2);
 insert into cidade (id, nome, estado_id) values (4, 'Campinas', 2);
 insert into cidade (id, nome, estado_id) values (5, 'Fortaleza', 3);
 
+SELECT setval('cidade_id_seq', 5, true);
+
 insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
 insert into forma_pagamento (id, descricao) values (2, 'Cartão de débito');
 insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
 
+SELECT setval('forma_pagamento_id_seq', 3, true);
+
 insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
 insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
+
+SELECT setval('permissao_id_seq', 2, true);
